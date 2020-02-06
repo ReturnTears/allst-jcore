6a73724c768ddd9522be6753198cdddbd0039b28
returntears <returntears@outlook.com>
2020-02-07 00:24:27 +0800

    java basic

---
+++ src/main/java/com/allst/jcore/jv8/AllstJv8Func.java
@@ -0,0 +1,26 @@
+package com.allst.jcore.jv8;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author YiYa
+ * @since 2020-02-04 下午 04:40
+ */
+public class AllstJv8Func {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+
+        File[] hiddenFiles = new File(".").listFiles(File::isHidden);
+        for (int i = 0; i < hiddenFiles.length; i++) {
+            System.out.println(hiddenFiles[i].getName());
+        }
+    }
+
+
+
+}
