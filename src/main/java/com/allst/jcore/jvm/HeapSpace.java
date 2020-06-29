0c9414cd971bcf858ddcd20702fa7dfd49629f85
returntears <returntears@outlook.com>
2020-06-29 23:36:27 +0800

    JVM 堆空间核心概述

---
+++ src/main/java/com/allst/jcore/jvm/HeapSpace.java
@@ -0,0 +1,19 @@
+package com.allst.jcore.jvm;
+
+/**
+ * 堆空间
+ * 通过两个不同的类进行测试JVM进程的堆空间独享
+ * @author YiYa
+ * @since 2020-06-29 下午 10:48
+ */
+public class HeapSpace {
+    public static void main(String[] args) {
+        System.out.println("start......");
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end........");
+    }
+}
