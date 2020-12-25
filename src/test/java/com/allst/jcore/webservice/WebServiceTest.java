9088b49e7e41775c2aa2b820769df68d84a93053
returntears <returntears@outlook.com>
2020-12-25 20:41:41 +0800

    Java获取系统参数

--- src/test/java/com/allst/jcore/webservice/WebServiceTest.java
+++ src/test/java/com/allst/jcore/webservice/WebServiceTest.java
@@ -1,21 +1,16 @@
 package com.allst.jcore.webservice;
 
-import com.allst.jcore.webservice.ws.HelloWSImpl;
-import org.junit.Test;
-
-import javax.xml.ws.Endpoint;
-
 /**
  * @author YiYa
  * @since 2020-10-20 下午 11:57
  */
 public class WebServiceTest {
 
-    @Test
+    /*@Test
     public void test() {
         String address = "http://192.168.31.72:8989/allst-ws/hellows";
         Endpoint.publish(address, new HelloWSImpl());
         System.out.println("发布webservices成功！");
-    }
+    }*/
 
 }
