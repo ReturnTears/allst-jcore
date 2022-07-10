0a5334bd19ab3e0335c464e1d0688d4cfbf69f43
returntears <returntears@outlook.com>
2022-07-10 19:43:29 +0800

    Java集合 - ArrayList扩容

--- src/main/java/com/allst/jcore/coll/CollArrayList.java
+++ src/main/java/com/allst/jcore/coll/CollArrayList.java
@@ -1,5 +1,7 @@
 package com.allst.jcore.coll;
 
+import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,12 @@ import java.util.List;
  */
 public class CollArrayList {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>(10);
+        list.add("Java");
+        list.add(1, "Scala"); // 指定位置添加元素
+        list.set(0, "Go");  // 指定位置替换元素，返回旧元素
+        System.out.println(list.size());
+        list.forEach(System.out::println);
+        List<String> lists = Lists.newArrayList();
     }
 }
