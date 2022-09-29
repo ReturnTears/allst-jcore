2239d56cd5cab7dd155e769ea4c57fe33e52a643
returntears <returntears@outlook.com>
2022-09-29 23:12:37 +0800

    java9

---
+++ src/main/java/com/allst/jcore/jv9/Jv9Demo.java
@@ -0,0 +1,9 @@
+package com.allst.jcore.jv9;
+
+/**
+ * @author Hutu
+ * @since 2022-09-29 下午 11:11
+ */
+public class Jv9Demo {
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author Hutu
+ * @since 2022-09-29 下午 11:09
+ */
+module allst.jcore {
+     requires java.base;
+     requires java.sql;
+}
