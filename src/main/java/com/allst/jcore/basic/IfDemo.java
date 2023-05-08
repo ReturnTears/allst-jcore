396146efa766b73da5a7d0fa7fc38427a4283ca9
returntears <returntears@outlook.com>
2023-05-08 22:13:20 +0800

    Java基础复习

---
+++ src/main/java/com/allst/jcore/basic/IfDemo.java
@@ -0,0 +1,23 @@
+package com.allst.jcore.basic;
+
+/**
+ * @author Hutu
+ * @since 2023-04-06 下午 05:00
+ */
+public class IfDemo {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num > 1) {
+            System.out.println("num > 1");
+        }
+        if (num > 5) {
+            System.out.println("num > 5");
+        }
+        if (num > 8) {
+            System.out.println("num > 8");
+        }
+        if (num > 10) {
+            System.out.println("num > 10");
+        }
+    }
+}
---
+++ src/main/java/com/allst/jcore/java/JavaWrapper.java
@@ -0,0 +1,13 @@
+package com.allst.jcore.java;
+
+/**
+ * @author Hutu
+ * @since 2023-05-08 下午 09:52
+ */
+public class JavaWrapper {
+    public static void main(String[] args) {
+        Integer num = 10;
+        int i = num.intValue();
+
+    }
+}
